852dffd8a97eab88129d19de670cee335c08dc7a
David Ashley <david-ashley@hotmail.com>
2024-02-28 23:16:32 -0600

    dynamic arrays

---
+++ memory.c
@@ -0,0 +1,12 @@
+#include "memory.h"
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    if (newSize == 0) {
+        free(pointer);
+        return NULL;
+    }
+
+    void* result = realloc(pointer, newSize); // realloc std lib function
+    if (result == NULL) exit(1);
+    return result;
+}
