2970e70c081bc820aaecb2342822d7c0c0bf4d95
David Ashley <david-ashley@hotmail.com>
2024-02-29 21:36:05 -0600

    values

---
+++ value.h
@@ -0,0 +1,13 @@
+typedef double Value;
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+
+#endif
