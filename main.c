f7bfb74652e629c7080cfb544d7847cc82db6dcb
David Ashley <david-ashley@hotmail.com>
2024-03-04 17:17:38 -0600

    print line num

--- main.c
+++ main.c
@@ -7,10 +7,12 @@ int main(int argc, const char* argv[]) {
     initChunk(&chunk);
     
     int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT); // OP Code
-    writeChunk(&chunk, constant); // Operand i.e. value's address
+    writeChunk(&chunk, OP_CONSTANT, 123); // OP Code
+    writeChunk(&chunk, constant, 123); // Operand i.e. value's address
+    writeChunk(&chunk, OP_CONSTANT, 123); 
+    writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
