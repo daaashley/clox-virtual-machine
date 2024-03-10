b969442877bb299de7bfa57da4d5a9a0fa0ef505
David Ashley <david-ashley@hotmail.com>
2024-03-10 14:44:17 -0500

    binary ops

--- vm.c
+++ vm.c
@@ -31,6 +31,14 @@ static InterpretResult run() {
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
+    #define BINARY_OP(op) \
+        do { \
+            double b = pop(); \
+            double a = pop(); \
+            push(a op b); \
+        } while (false)
+        
+
     for (;;) {
         #ifdef DEBUG_TRACE_EXECUTION
         for (Value* slot = vm.stack; slot < vm.stackTop; slot++){
@@ -48,6 +56,11 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -57,6 +70,7 @@ static InterpretResult run() {
     }
     #undef READ_BYTE
     #undef READ_CONSTANT
+    #undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
