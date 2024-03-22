f83b1d475581f68a75e8a9cc1cad38a3ba7aa314
David Ashley <david-ashley@hotmail.com>
2024-03-22 14:33:57 -0500

    make token, error token

--- scanner.c
+++ scanner.c
@@ -25,6 +25,28 @@ static bool isAtEnd()
     return *scanner.current == '\0';
 }
 
+static Token makeToken(TokenType type)
+{
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+
+    return token;
+}
+
+static Token errorToken(const char *message)
+{
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = message;
+    token.length = (int)strlen(message);
+    token.line = scanner.line;
+
+    return token;
+}
+
 Token scanToken()
 {
     scanner.start = scanner.current;
